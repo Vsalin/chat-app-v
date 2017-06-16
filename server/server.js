0372d4fe0b32d1c3f45e1c46a8422d84503ea5d6
ja-PC\ja <vesalin.thw@g.swu.ac.th>
2017-06-16 08:55:26 +0700

    no message

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const http = require('http');
+const express = require('express'); 
+const socketIO = require('socket.io');
+const {generateMessage} = require('./untils/message')
+const publicPath = path.join(__dirname,'../public');
+const port =process.env.PORT || 3000
+
+var app = express();
+var server= http.createServer(app)
+var io = socketIO(server)
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New User connected');
+
+socket.emit('newMessage',generateMessage ('Admin','Welcome tochat'))
+  
+socket.broadcast.emit('newMessage',generateMessage ('Admin','NewUser Join'))
+
+socket.on('createMessage', (message,callback)=>{
+     console.log('createMessage',message);
+    io.emit('newMessage',generateMessage (message.from,message.text))
+    callback('This is from server')
+    // socket.broadcast.emit('newMessage',{
+    //         from:message.from,
+    //         text:message.text,
+    //     createAt: newDate().getTime()
+    // })
+});
+
+socket.on('disconnect', () => {
+    console.log('User disconnect server');
+   });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
+
+
+
